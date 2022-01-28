04623bf7949cd05137e3b636adbe505afd41db59
Юлия Шепелева <9128051206@mail.ru>
2022-01-29 01:26:29 +0500

    Добавьте файлы проекта.

---
+++ Lab16-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+
+namespace Lab162 
+{
+    
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+              string jsonstring = String.Empty;
+              using (StreamReader sr = new StreamReader("../../../Products.json"))
+              {
+                   jsonstring = sr.ReadToEnd();
+              }
+               Product[] products = JsonSerializer.Deserialize<Product[]>(jsonstring);
+
+               Product maxprroduct = products[0];
+               foreach(Product e in products)
+               {
+                  if (e.Price>maxprroduct.Price)
+                  {
+                    maxprroduct = e;
+                  }
+               }
+               Console.WriteLine($"{maxprroduct.Code} {maxprroduct.Title} {maxprroduct.Price}");
+               Console.ReadKey();
+            }
+        }
+ }
---
+++ Lab16/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab16
+{
+    class Product
+    {
+        public int Code { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+
+
+
+
+    }
+}
